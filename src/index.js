7ec52badab055511b41aee559d446fb0ef22bda0
Isale <isale.bryan@gmail.com>
2020-09-21 10:48:33 +0300

    Code splitting with dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import moment from 'moment';
+// USING ES6 MODULES
+// import { getUsers } from './common/usersAPI';
+
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', () => {
+//   getUsers().then(json => console.log(json));
+// })
+
+// USING DYNAMIC IMPORTS
+//  create a function to load the module dynamically
+const getUserModule = () => import(/* webpackChunkName: "usersAPI" */'./common/usersAPI');
+
+const btn = document.getElementById('btn');
+
+// in the event listener we chain then() to the dynamic import
+btn.addEventListener('click', () => {
+  // This gives the ability to extract our getUsers function with object destructuring
+  getUserModule().then(({ getUsers }) => {
+    getUsers().then(json => console.log(json));
+  });
+});
