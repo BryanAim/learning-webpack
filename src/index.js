49b77833fddb625baf415eaf6b48ff39c671b36a
Isale <isale.bryan@gmail.com>
2020-09-21 10:04:04 +0300

    Working with JavaScript's modules in webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { getUsers } from './common/usersAPI';
 // import './style.css';
 import './style.scss'
 console.log('Hello Webpack!');
+
+getUsers().then(json => console.log(json));
 
 const fancyFunc = () => {
   return [1,2];
